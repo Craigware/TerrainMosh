4a910393b235317229dbe6d3c96a85c865e954b0
Craig <craigjart@gmail.com>
2024-06-18 10:30:09 -0400

    Working on the player controller

--- Scripts/Controllers/Player.cs
+++ Scripts/Controllers/Player.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 namespace Controllers 
 {
@@ -8,51 +7,106 @@ namespace Controllers
         public enum PlayerStates {
             Neutral,
             Sweeping,
+            ChargingRoll,
             Rolling,
             Backstepping,
             Throwing,
             Healing
         }
 
-        public int Health = 100;
-        public int Stamina = 100;
-        public float MoveSpeed = 200;
+        [Export] public int Health = 100;
+        [Export] public int Stamina = 100;
+        public bool BurnedOut = false;
+ 
+        public float WalkSpeed = 100;
+        [Export] public float MoveSpeed = 200;
+        
+
+        float MaxMoveSpeedTime = 0.5f;
+        float MaxMoveSpeedProgress = 0;
 
         public PlayerStates State = PlayerStates.Neutral;
         Vector2 facing = Vector2.Down;
 
-        public int CostToRoll = 20;
-        public int CostToBackstep = 20;
+        Vector2 movementVelocity;
 
-        public bool BurnedOut = false;
+
+        [Export] public float RollSpeed = 800;
+        float RollStrength = 1f;
+        float RollTime = 0.25f;
+        float RollProgress = 0;
+        public int CostToRoll = 20;
+
+        [Export] public float BackSteepStrength = 400;
+        float BackstepTime = 0.1f;
+        float BackstepProgress = 0;
+        public int CostToBackstep = 20;
 
         public override void _PhysicsProcess(double delta)
         {
-            Vector2 movementDirection = Input.GetVector("Left", "Right", "Up", "Down");
-            if (movementDirection != Vector2.Zero) {
-                var movementVelocity = movementDirection * MoveSpeed * (float) delta;
-                MoveAndCollide(movementVelocity);
+            if (State != PlayerStates.Rolling && State != PlayerStates.Backstepping) {
+                Vector2 movementDirection = Input.GetVector("Left", "Right", "Up", "Down");
+ 
+                if (movementDirection != Vector2.Zero) {
+                    movementVelocity = movementDirection * (MoveSpeed * (1 + MaxMoveSpeedProgress)) * (float) delta;
+                    MoveAndCollide(movementVelocity);
+                    MaxMoveSpeedProgress = Mathf.Clamp(MaxMoveSpeedProgress + 1 * (float) delta, 0, 0.5f);
+                } else {
+                    MaxMoveSpeedProgress = 0;
+                }
+
+                facing = Position.DirectionTo(GetViewport().GetMousePosition());
+            }
+            
+            if (State == PlayerStates.Rolling) {
+                MoveAndCollide(movementVelocity * (float) delta);
+                
+                if (RollProgress >= RollTime) {
+                    State = PlayerStates.Neutral;
+                    RollProgress = 0;
+                } else 
+                    RollProgress += 1 * (float) delta;
+            }
+
+            if (State == PlayerStates.Backstepping) {
+                MoveAndCollide(movementVelocity * (float) delta);
+                if (BackstepProgress >= BackstepTime) {
+                    State = PlayerStates.Neutral;
+                    BackstepProgress = 0;
+                } else 
+                    BackstepProgress += 1 * (float) delta;
+            }
+
+            if (State == PlayerStates.ChargingRoll) {
+                RollStrength = Mathf.Clamp(RollStrength + 1 * (float) delta, 1, 1.5f);
             }
         }
 
         public override void _Input(InputEvent @event) {
             if (BurnedOut) return;
-
-            if (Input.IsActionJustPressed("Roll")) {
-                if (State == PlayerStates.Neutral || State == PlayerStates.Healing) {
-                    Roll();
-                }
-            }
+            if (Input.IsActionJustPressed("Roll")) BeginRoll();
+            if (Input.IsActionJustReleased("Roll")) Roll();
+            if (Input.IsActionJustPressed("Backstep")) Backstep();
         }
 
-
-
+        public void BeginRoll() {
+            if (State == PlayerStates.Rolling) return;
+            State = PlayerStates.ChargingRoll;
+        }
 
         public void Roll() {
-            if (State == PlayerStates.Rolling) return;
+            if (State != PlayerStates.ChargingRoll) return;
+            State = PlayerStates.Rolling;
+            movementVelocity = facing * RollSpeed * RollStrength;
+            RollStrength = 1;
         }
 
-        public void Backstep() {}
+        public void Backstep() {
+            if (State == PlayerStates.Backstepping) return;
+            State = PlayerStates.Backstepping;
+            movementVelocity = facing * -1 * RollSpeed;
+        }
+ 
         public void Attack() {}
         public void Throw() {}
         public void Heal() {}
@@ -66,10 +120,9 @@ namespace Controllers
 
         public void RecoverStamina(int amount) {
             Stamina = Mathf.Clamp(Stamina + amount, 0, 100);
-            if (Stamina == 100) {
+            if (Stamina == 20) {
                 if (BurnedOut) BurnedOut = false;
             }
         }
-
     }
 }
